0e9941bd6f37c321be61ebca90e1cd33fed1020f
aminulhaque98 <aminulhaque419@gmail.com>
2023-06-19 23:10:48 +0530

    bot is complate

--- src/Pages/Bot/Bot.js
+++ src/Pages/Bot/Bot.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BsChatDotsFill } from 'react-icons/bs';
 
 const Bot = () => {
     return (
-        <div>
-            <h2>bot page</h2>
+        <div className='fixed bottom-5 sm:right-8 right-4 z-[999] cursor-point text-white text-4xl bg-cyan-600 w-16 h-16 flex items-center justify-center rounded-full animate-bounce'>
+            <BsChatDotsFill></BsChatDotsFill>
         </div>
     );
 };
