0f1352c9197c2e2ba25f97d3a626c2d09efbbcc2
aminulhaque98 <aminulhaque419@gmail.com>
2023-06-20 23:35:42 +0530

    modify navbar

---
+++ src/Pages/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className='h-screen'>
+            <h2>lorem000</h2>
+        </div>
+    );
+};
+
+export default Hero;
